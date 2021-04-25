8040df04298c9225e4f6d5fab509de72eea47934
Janis Lebron <janislebron@gmail.com>
2021-04-25 14:50:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,11 +24,11 @@ class Application
     end
   elsif req.path.match(/add/)
     add_item = req.params["item"]
-   
+
     if @@items.include?(add_item)
         @@cart << add_item
         resp.write "added #{add_item}"
-     
+
       else
         resp.write "We don't have that item"
       end
